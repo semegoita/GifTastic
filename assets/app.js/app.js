f509201e486845cef0ca2a15f094731d1d7f9700
Semere G <semere@Semeres-MBP.attlocal.net>
2018-04-05 14:34:50 -0700

    Pause function included

--- assets/app.js/app.js
+++ assets/app.js/app.js
@@ -3,10 +3,10 @@ var animals = ['Cat', 'Lion', 'Zebra', 'Elephant'];
 
 function displayGif() {
   var animal = $(this).attr("data-name");
-  //console.log("yes");
+  var state = $(this).attr("data-state");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     animal + "&api_key=9qCIOv455he8jseNY6sxmFOQfROgvWZ6";
-  //console.log("yay");
+
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -18,30 +18,45 @@ function displayGif() {
     for (var i = 0; i < 10; i++) {
       // Creating and storing a div tag
       var animalDiv = $("<div>");
-
       // Creating a paragraph tag with the result item's rating
       var p = $("<p>").text("Rating: " + results[i].rating);
 
       // Creating and storing an image tag
       var animalImage = $("<img>");
+      animalImage.addClass = ("anImg");
       // Setting the src attribute of the image to a property pulled off the result item
-      animalImage.attr("src", results[i].images.fixed_height.url);
-      // if (animal === "still") {
-      //   $(this).attr("src", $(this).attr("data-animate"));
-      //   $(this).attr("data-state", "animate");
-      // } else {
-      //   $(this).attr("src", $(this).attr("data-still"));
-      //   $(this).attr("data-state", "still");
-      // }
-
+      animalImage.attr('src', results[i].images.fixed_height.url);
+      animalImage.attr('data-still', results[i].images.fixed_height_still.url);
+      animalImage.attr('data-animate', results[i].images.fixed_height.url);
+      //.attr('data-state', 'still');;
       // Appending the paragraph and image tag to the animalDiv
       animalDiv.append(p);
       animalDiv.append(animalImage);
 
       // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
       $("#gif-view").prepend(animalDiv);
-     
+      $('#gif-view > div:nth-child(n) > img').on('click', function () {
+        //console.log('yes');
+        // if (state == 'still') {
+        //   $(this).attr('src', $(this).data('data-animate'));
+        //   $(this).attr('data-state', 'animate');
+        // } else {
+        //   $(this).attr('src', $(this).data('data-still'));
+        //   $(this).attr('data-state', 'still');
+        // }
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+
+      })
+
+
     }
+
     renderButtons();
   })
 }
@@ -71,7 +86,11 @@ renderButtons();
 
 
 
- //api key = 9qCIOv455he8jseNY6sxmFOQfROgvWZ6
+
+
+
+
+
 
 
 
